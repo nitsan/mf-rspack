9acb5e6ef9b543f2c3fcb4c60a29a6017d6100cc
nitsan <nitsan@next-insurance.com>
2025-11-15 19:53:01 +0200

    add lead_form

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'lead_form',
+    loadChildren: () => import('lead_form/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
