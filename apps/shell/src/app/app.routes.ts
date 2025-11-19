12c6b5f3a441656b79930296a68dad674c67bdde
nitsan <nitsan@next-insurance.com>
2025-11-19 14:58:59 +0200

    add guides remote app

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'guides',
+    loadChildren: () => import('guides/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'hub',
     loadChildren: () => import('hub/Routes').then((m) => m!.remoteRoutes),
   },
