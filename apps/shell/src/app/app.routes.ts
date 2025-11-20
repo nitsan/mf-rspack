771467d083ee8073aea9545fc24ea6d4518a1f30
nitsan <nitsan@next-insurance.com>
2025-11-20 21:41:23 +0200

    add hub

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'hub',
+    loadChildren: () => import('hub/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'guides',
     loadChildren: () => import('guides/Routes').then((m) => m!.remoteRoutes),
   },
