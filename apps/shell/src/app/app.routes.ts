879a8172863de22209c6fc6067154a7b5906dfa8
nitsan <nitsan@next-insurance.com>
2025-11-20 22:19:54 +0200

    replace order

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'hub',
-    loadChildren: () => import('hub/Routes').then((m) => m!.remoteRoutes),
-  },
-  {
     path: 'guides',
     loadChildren: () => import('guides/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: 'hub',
+    loadChildren: () => import('hub/Routes').then((m) => m!.remoteRoutes),
   },
   {
     path: '',
