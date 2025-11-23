0981f0b4f4fc9765bee549f8d5de79207f2406a2
nitsan <nitsan@next-insurance.com>
2025-11-23 15:45:16 +0200

    push rspack.config.ts of guides

---
+++ apps/guides/rspack.config.ts
@@ -0,0 +1,66 @@
+import { createConfig } from '@nx/angular-rspack';
+import baseWebpackConfig from './webpack.config';
+import webpackMerge from 'webpack-merge';
+
+export default async () => {
+  const baseConfig = await createConfig(
+    {
+      options: {
+        root: __dirname,
+
+        outputPath: {
+          base: '../../dist/apps/guides',
+          browser: '../../dist/apps/guides',
+        },
+        index: './src/index.html',
+        browser: './src/main.ts',
+        polyfills: ['zone.js'],
+        tsConfig: './tsconfig.app.json',
+        inlineStyleLanguage: 'scss',
+        assets: [
+          {
+            glob: '**/*',
+            input: './public',
+          },
+        ],
+        styles: ['./src/styles.scss'],
+        scripts: [],
+        devServer: {
+          port: 4201,
+        },
+      },
+    },
+    {
+      production: {
+        options: {
+          budgets: [
+            {
+              type: 'initial',
+              maximumWarning: '500kb',
+              maximumError: '1mb',
+            },
+            {
+              type: 'anyComponentStyle',
+              maximumWarning: '4kb',
+              maximumError: '8kb',
+            },
+          ],
+          outputHashing: 'all',
+          devServer: {},
+        },
+      },
+
+      development: {
+        options: {
+          optimization: false,
+          vendorChunk: true,
+          extractLicenses: false,
+          sourceMap: true,
+          namedChunks: true,
+          devServer: {},
+        },
+      },
+    }
+  );
+  return webpackMerge(baseConfig[0], baseWebpackConfig);
+};
